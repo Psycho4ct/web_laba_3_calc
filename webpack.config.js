86e5b6f2bca0de599771522f3c54ea7f9131480b
Felist dsa <vadimgalk@mail.ru>
2024-04-22 23:30:39 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+module.exports= {
+    mode : 'development',
+
+    devtool: 'source-map',
+
+    module: {
+        rules: [
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+          }
+        ]
+      },
+      module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
+
+      devServer: {
+        static: './dist',
+      },
+
+      plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({ template: './index.html' }),
+      ],
+}
